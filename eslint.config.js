83586ae3081a434cc25b178aa46d5345cc31063e
Trung Dang <trung.dangquoc@gameloft.com>
2023-11-03 14:13:03 +0000

    chore: prefill mapping for ignores and rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,16 @@
 import unocss from '@unocss/eslint-config/flat'
 import antfu from '@antfu/eslint-config'
 
-export default antfu({}, unocss)
+export default antfu(
+  {
+    ignores: [
+      // eslint ignore globs here
+    ],
+  },
+  {
+    rules: {
+      // overrides
+    },
+  },
+  unocss,
+)
