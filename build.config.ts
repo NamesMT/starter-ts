68a9ed5501eaee82b218ff28cebbcc1d69294605
Trung Dang <dangquoctrung123@gmail.com>
2023-11-24 01:57:16 +0000

    Changes for reproduce mkdist alias

--- build.config.ts
+++ build.config.ts
@@ -4,14 +4,14 @@ import sharedConfig from './shared.config'
 export default defineBuildConfig({
   entries: [
     // normal bundles
-    'src/index',
+    // 'src/index',
 
     // file-to-file (please use .mts for all of your files), currently mkdist uncontrollably generates .d.ts and .d.mts based on the original extension: .ts or .mts
-    // {
-    //   builder: 'mkdist',
-    //   input: './src/',
-    //   esbuild: { minify: true },
-    // },
+    {
+      builder: 'mkdist',
+      input: './src/',
+      esbuild: { minify: true },
+    },
   ],
   declaration: 'node16',
   clean: true,
