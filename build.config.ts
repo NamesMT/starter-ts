3b2ef7b17cab3bf4142580f49b353782194d4d38
NamesMT <dangquoctrung123@gmail.com>
2025-04-12 15:18:19 +0000

    docs: adjust comment a bit

--- build.config.ts
+++ build.config.ts
@@ -6,7 +6,7 @@ export default defineBuildConfig({
     // normal bundles
     'src/index',
 
-    // file-to-file (please use .mts for all of your files), currently mkdist uncontrollably generates .d.ts and .d.mts based on the original extension: .ts or .mts
+    // file-to-file using `mkdist`, note: currently mkdist uncontrollably generates .d.ts and .d.mts based on the original extension: .ts or .mts
     // {
     //   builder: 'mkdist',
     //   input: './src/',
