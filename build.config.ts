03783548a56f8d2ee4fb4edf47bab89eb55e5069
NamesMT <dangquoctrung123@gmail.com>
2025-04-29 09:51:53 +0000

    chore(build): prefer auto interference by default

--- build.config.ts
+++ build.config.ts
@@ -2,17 +2,17 @@ import { defineBuildConfig } from 'unbuild'
 import sharedConfig from './shared.config'
 
 export default defineBuildConfig({
-  entries: [
-    // normal bundles
-    'src/index',
+  // entries: [
+  //   // normal bundles
+  //   'src/index',
 
-    // file-to-file using `mkdist`, note: currently mkdist uncontrollably generates .d.ts and .d.mts based on the original extension: .ts or .mts
-    // {
-    //   builder: 'mkdist',
-    //   input: './src/',
-    //   esbuild: { minify: true },
-    // },
-  ],
+  //   // file-to-file using `mkdist`, note: currently mkdist uncontrollably generates .d.ts and .d.mts based on the original extension: .ts or .mts
+  //   // {
+  //   //   builder: 'mkdist',
+  //   //   input: './src/',
+  //   //   esbuild: { minify: true },
+  //   // },
+  // ],
   declaration: 'node16',
   clean: true,
   rollup: {
