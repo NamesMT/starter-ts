21732094e02f6f06833251827c1e1bca361d3533
NamesMT <dangquoctrung123@gmail.com>
2025-05-10 13:44:46 +0000

    chore: housekeep

--- eslint.config.js
+++ eslint.config.js
@@ -2,21 +2,27 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu(
   {
+    // Type of the project. 'lib' | 'app' (default 'app')
+    type: 'lib',
+
+    // Customize the stylistic rules
+    stylistic: {
+      indent: 2, // 4, or 'tab'
+      quotes: 'single', // or 'double'
+    },
+
+    // TypeScript and Vue are autodetected, you can also explicitly enable them:
+    typescript: true,
+    // vue: true,
+
+    // ESLint ignore globs here
     ignores: [
-      // eslint ignore globs here
     ],
   },
   {
     rules: {
       // Relaxes inline statements a bit
       'style/max-statements-per-line': ['error', { max: 2 }],
-    },
-  },
-  // Allow trailing space for markdown formatting
-  {
-    files: ['**/*.md'],
-    rules: {
-      'style/no-trailing-spaces': 'off',
     },
   },
 )
