ac63500366b57814589e30cf561b4a6593178361
NamesMT <dangquoctrung123@gmail.com>
2025-07-08 08:31:20 +0000

    chore: add note for `helpers/logger`

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -2,6 +2,11 @@ import type { ConsolaInstance } from 'consola'
 import { createConsola, LogLevels } from 'consola'
 import { isDevelopment } from 'std-env'
 
+/**
+ * Note: this logger will log the `debug` level logs in development mode.
+ *
+ * For actual debug logs with `NODE_DEBUG`, it is recommended to use the `debug` package.
+ */
 export const logger: ConsolaInstance = createConsola(
   {
     level: isDevelopment ? LogLevels.debug : undefined,
