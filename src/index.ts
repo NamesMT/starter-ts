8624e9928d4ae7a1ba1200fced18b015a48aa4d7
NamesMT <dangquoctrung123@gmail.com>
2025-11-20 23:04:05 +0700

    chore: add repro

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
+import { deepMerge } from '@antfu/utils'
+
 export const shouldBe200 = 200
+
+interface TypedInterface {
+  str: string
+  num: number
+}
+
+const strMerge = deepMerge(
+  { str: 'abc' },
+  { str: 'xyz' },
+)
+const numMerge = deepMerge(
+  { num: 1 },
+  { num: 2 },
+)
+
+const typedInterfaceMerge = deepMerge(
+  { str: 'abc', num: 1 } as TypedInterface,
+  { str: 'xyz', num: 2 } as TypedInterface,
+)
